ba0a8aa044f2c851b298e41b8c8dbf982f619d3f
Marc Bigler <mbigler@cmpdesigners.com>
2017-10-26 04:12:25 +0000

    Polls app is brought up to hello world speed

--- cs_2610/urls.py
+++ cs_2610/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^hello_world/', include('hello_world.urls')),
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('hello_world.urls')),
+    url(r'^', include('polls.urls')),
 ]
---
+++ polls/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+import time
+
+def index(request):
+    now = time.strftime('%c')
+    return HttpResponse("Hello, you are in the polls index. \n Time is %s" % now)
+
+# Create your views here.
