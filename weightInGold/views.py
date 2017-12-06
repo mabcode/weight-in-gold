c28a554e0bc79f072c6246ae12963af445c1f197
Marc Bigler <mbigler@cmpdesigners.com>
2017-12-06 20:56:26 +0000

    assignment is working, Not on difference apps

--- weightInGold/views.py
+++ weightInGold/views.py
@@ -2,27 +2,17 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse,JsonResponse
 from django.views import generic
 from django.urls import reverse
 
 from .models import Unit
 import json 
 
-
-#class HomeView(generic.ListView):
-#    template_name = 'weightInGold/home.html'
-#    context_object_name = 'input'
-#    def get_queryset(self):
-#        Home.objects.order_by('-posted_date')
-        
-def computeWeight(n):
-	if (n == 0):
-		return 0
-	elif (n == 1):
-		return 1
-	else:
-		return computeWeight(n-1) + computeWeight(n-2)
+def computeWeight(from_unit,to_unit,amount):
+	target_unit = Unit.objects.get(name=to_unit)
+	source_unit = Unit.objects.get(name=from_unit)
+	return target_unit.basis_factor * source_unit.basis_factor * amount
 
 
 def weightAPI(request):
@@ -38,10 +28,10 @@ def weightAPI(request):
 		if (n < 0):
 			resp['error'] = "Usage: n=[non-negative integer]"
 		else:
-			val = computeWeight(n)
+			val = computeWeight('pound','tory_ounce',n)
 			resp = { 'n': n, 'computeWeight': val }
 
-	return HttpResponse(json.dumps(resp))
+	return JsonResponse(resp)
 
 def index(request):
     return render(request, 'weightInGold/home.html', {})
